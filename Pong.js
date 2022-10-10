fee3d8ef4c57178e05846f579ef090a1fb18aacd
daniel-boxall <104602940+daniel-boxall@users.noreply.github.com>
2022-10-11 00:36:47 +0100

    added touch control

--- Pong.js
+++ Pong.js
@@ -42,6 +42,10 @@ function handleLose() {
     computerPaddle.reset()
 }
 
+document.addEventListener("touchmove", e => {
+    playerPaddle.position = (e.y / window.innerHeight) * 100
+})
+
 document.addEventListener("mousemove", e => {
     playerPaddle.position = (e.y / window.innerHeight) * 100
 })
