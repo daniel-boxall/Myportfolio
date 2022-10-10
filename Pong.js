a253038b9aa81adaf6752a4ea86c809b5e954be5
daniel-boxall <104602940+daniel-boxall@users.noreply.github.com>
2022-10-11 00:57:23 +0100

    test

--- Pong.js
+++ Pong.js
@@ -43,7 +43,7 @@ function handleLose() {
 }
 
 
-document.addEventListener("pointer", e => {
+document.addEventListener("pointermove", e => {
     playerPaddle.position = (e.y / window.innerHeight) * 100
 })
 
